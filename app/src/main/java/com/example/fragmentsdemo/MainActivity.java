fa3e442e9e5813afa5c8b8011c62d7f3beacb52d
kivancuzer <kivancuzer@gmail.com>
2020-09-24 14:46:18 +0300

    fragmentTransaction

--- app/src/main/java/com/example/fragmentsdemo/MainActivity.java
+++ app/src/main/java/com/example/fragmentsdemo/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.fragmentsdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentTransaction fragmentTransaction;
+    MessagesFragment messagesFragment;
+    ContactsFragment contactsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        messagesFragment = new MessagesFragment();
+        contactsFragment = new ContactsFragment();
+
+        //When Activity created first time
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frameLayout, messagesFragment, "Some tag").commit();
+        }
+
+
+    }
+
+    /**
+     * Go To Messages Fragment
+     * <p>
+     * When fragmentMessages button clicked
+     *
+     * @param view
+     */
+    public void goToMessages(View view) {
+
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, messagesFragment).commit();
+
+    }
+
+    /**
+     * Go To Contacts Fragment
+     * <p>
+     * When fragmentContacts button clicked
+     *
+     * @param view
+     */
+    public void goToContacts(View view) {
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, contactsFragment).commit();
     }
 }
--- app/src/main/java/com/example/fragmentsdemo/MessagesFragment.java
+++ app/src/main/java/com/example/fragmentsdemo/MessagesFragment.java
@@ -1,29 +1,80 @@
 package com.example.fragmentsdemo;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MessagesFragment extends Fragment {
-
+    //ThingsAdapter adapter;
+    FragmentActivity listener;
 
     public MessagesFragment() {
         // Required empty public constructor
     }
 
+    //This event fires 1st, before creation of fragment or any views
+    //The onAttach method is called when the Fragment instance is associated with an Activity.
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof Activity) {
+            this.listener = (FragmentActivity) context;
+        }
+    }
+
+    //This event fires 2nd, before views are created for the fragment.
+    //The onCreate method is called when the Fragment instance is being created, or re-created
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //   ArrayList<Thing> things = new ArrayList<>();
+        //   adapter = new ThingsAdapter(getActivity(), things);
     }
 
+    // The onCreateView method is called when Fragment should create its View object hierarchy,
+    // either dynamically or via XML layout inflation.
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_messages, container, false);
+    }
+
+    //This event is triggered soon after onCreateView()
+    //onViewCreated() is only called if the view returned from onCreateView() is non-null.
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // ListView lv = (ListView) view.findViewById(R.id.lvSome);
+        // lv.setAdapter(adapter);
+    }
+
+    // This method is called when the fragment is no longer connected to the Activity
+    // Any references saved in onAttach should be nulled out here to prevent memory leaks.
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        this.listener = null;
+    }
+
+    // This method is called after the parent Activity's onCreate() method has completed.
+    // Accessing the view hierarchy of the parent activity must be done in the onActivityCreated.
+    // At this point, it is safe to search for activity View objects by their ID, for example.
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
     }
 }
